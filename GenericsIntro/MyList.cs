ae03915cb8e2d05abf3041f55d89f6a401a2f5c5
Mehmet Ali CİRİT <mehmetalicirit@hotmail.com>
2021-12-04 16:56:22 +0300

    //uzak Depo constructor R7 ERA

--- GenericsIntro/MyList.cs
+++ GenericsIntro/MyList.cs
@@ -7,7 +7,7 @@ namespace GenericsIntro
     class MyList<T>
     {
         T[] items;
-        //constructor R5 ERA
+        //constructor R7 ERA
         public MyList()
         {
             items = new T[0];
